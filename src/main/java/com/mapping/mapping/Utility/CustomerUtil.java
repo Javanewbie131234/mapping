958702e020e8e4c951f9bfd773ddc9191e66fde1
Himalaya S <himalayah270@gmail.com>
2024-04-13 21:30:36 +0530

    Fixed null issue.

--- src/main/java/com/mapping/mapping/Utility/CustomerUtil.java
+++ src/main/java/com/mapping/mapping/Utility/CustomerUtil.java
@@ -2,6 +2,10 @@ package com.mapping.mapping.Utility;
 
 import com.mapping.mapping.Dto.CustomerDTO;
 import com.mapping.mapping.entity.Customer;
+import com.mapping.mapping.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CustomerUtil {
 
@@ -10,33 +14,43 @@ public class CustomerUtil {
         customer.setId(customerDTO.getId());
         customer.setName(customerDTO.getName());
         customer.setEmailId(customerDTO.getEmailId());
-//        if(customerDTO.getProduct()!=null){
-//            List<Product> products = new ArrayList<>();
-//            for(Product product : customerDTO.getProduct()){
-//                products.add(product);
-//            }
-//
-//            customer.setProduct(products);
-//        }
+        if(customerDTO.getProduct()!=null){
+            List<Product> products = new ArrayList<>();
+            for(Product product : customerDTO.getProduct()){
+               Product addProduct = new Product();
+            addProduct.setProductId(product.getProductId());
+            addProduct.setProductName(product.getProductName());
+                addProduct.setPrice(product.getPrice());
+                addProduct.setQuantity(product.getQuantity());
+            products.add(addProduct);
+            }
+
+            customer.setProduct(products);
+        }
         return  customer;
 
     }
 
-    public static CustomerDTO convertToDTO(Customer customer){
-    CustomerDTO customerDTO = new CustomerDTO();
-    customerDTO.setId(customer.getId());
-    customerDTO.setName(customer.getName());
-    customerDTO.setEmailId(customer.getEmailId());
-//    if(customer.getProduct()!=null){
-//        List<Product> productsToAdd = new ArrayList<>();
-//        for(Product product : customer.getProduct()){
-//            productsToAdd.add(product);
-//        }
-//        customerDTO.setProduct(productsToAdd);
+    public static CustomerDTO convertToDTO(Customer customer) {
+        CustomerDTO customerDTO = new CustomerDTO();
+        customerDTO.setId(customer.getId());
+        customerDTO.setName(customer.getName());
+        customerDTO.setEmailId(customer.getEmailId());
+        if (customer.getProduct() != null) {
+            List<Product> productsToAdd = new ArrayList<>();
+            for (Product product : customer.getProduct()) {
+                Product addProduct = new Product();
+                addProduct.setProductId(product.getProductId());
+                addProduct.setProductName(product.getProductName());
+                addProduct.setPrice(product.getPrice());
+                addProduct.setQuantity(product.getQuantity());
+                productsToAdd.add(addProduct);
+            }
+            customerDTO.setProduct(productsToAdd);
+        }
 
-
-        return customerDTO;
-    }
+            return customerDTO;
+        }
 
 
 }
